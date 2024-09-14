fc36e2a84456278713f845c91c408ba3548c22e8
mulinatti <gabrielmulinari2002@gmail.com>
2024-09-14 16:19:25 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, ButtonText } from './components/ui/button';
+
+export default function App() {
+  return (
+    <GluestackUIProvider mode="light"><View style={styles.container}>
+      <Button size="md" variant="solid" action="primary">
+        <ButtonText>Hello World!</ButtonText>
+      </Button>
+    </View></GluestackUIProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
