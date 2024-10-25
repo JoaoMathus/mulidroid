5ddcbea5cef9c941b7a2d23227a2ff70fbab79f4
mulinatti <gabrielmulinari2002@gmail.com>
2024-10-25 17:42:42 -0300

    add: checkbox input cadastro ajudante

--- src/screens/ajudante-form.tsx
+++ src/screens/ajudante-form.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import Text from "../components/ui/text";
 import Input from "../components/ui/input";
 import Button from "../components/ui/button";
+import CheckBox from "../components/ui/checkbox";
 
 const AjudanteForm = () => {
   return (
@@ -13,6 +14,7 @@ const AjudanteForm = () => {
       <Input label="Telefone"/>
       <Input label="Data de Nascimento"/>
       <Input label="Usuário"/>
+      <CheckBox/>
       <Button className="bg-blue-500 p-4 rounded-md mt-4">
         <Text className="text-lg text-center text-white" weight="semiBold">Cadastrar</Text>
       </Button>
