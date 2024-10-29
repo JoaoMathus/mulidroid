28a857796fd9b896752736a3048c6b143c04fc38
JoaoMathus <joaomatheusGDO@gmail.com>
2024-10-29 20:12:55 -0300

    Lembretes de teste para a tela de login.

--- src/screens/login.spec.js
+++ src/screens/login.spec.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render, screen, getByLabelText } from '@testing-library/react-native';
 import Login from './login';
 
+/**
+ * Quando 'esqueceu a senha?' estiver implementado, testá-lo.
+ * Quando o botão de login estiver implementado, testá-lo.
+ * Quando estiver pronta a conexão com o banco, testá-la.
+ */
 describe('Login', () => {
     it('mostra a logo do projeto', () => {
         render(<Login />);
