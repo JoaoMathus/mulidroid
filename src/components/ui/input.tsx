3082df2bf12f372a7b03661c067a7d035a701371
JoaoMathus <joaomatheusGDO@gmail.com>
2024-10-31 13:36:04 -0300

    Modificação de <Input> para pegar input do usuário no <Login>.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,13 +6,15 @@ import Text from "./text";
 interface InputProps {
   label: string;
   secureTextEntry?: boolean;
+  onChangeText?: React.Dispatch<React.SetStateAction<string>>;
+  value?: string;
 }
 
-const Input = ({label, secureTextEntry}: InputProps) => {
+const Input = ({label, secureTextEntry, onChangeText, value}: InputProps) => {
   return (
     <View className="w-full relative">
       <Text className="mb-2" weight="medium">{label}</Text>
-      <TextInput secureTextEntry={secureTextEntry} textContentType="password" cursorColor={"#0077ff"} className="placeholder:text-black/20 rounded-md border border-black/10 w-full py-4 px-4 text-xl" style={{ fontFamily: fontVariants["light"] }}/>
+      <TextInput secureTextEntry={secureTextEntry} onChangeText={onChangeText} value={value} textContentType="password" cursorColor={"#0077ff"} className="placeholder:text-black/20 rounded-md border border-black/10 w-full py-4 px-4 text-xl" style={{ fontFamily: fontVariants["light"] }}/>
     </View>
   )
 }
