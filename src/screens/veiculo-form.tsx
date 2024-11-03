a9d30d2f53e11a257f3daa6f4e634cf130650169
JoaoMathus <joaomatheusGDO@gmail.com>
2024-11-02 21:44:17 -0300

    Tela de cadastro de veículo: limpeza.

--- src/screens/veiculo-form.tsx
+++ src/screens/veiculo-form.tsx
@@ -10,7 +10,7 @@ import Button from "../components/ui/button";
  */
 const VeiculoForm = () => {
   return (
-    <View className="mt-10 w-full gap-5 p-8">
+    <View className="w-full gap-5 p-8">
       <Text className="text-left text-2xl" weight="black">Cadastro de Veículos</Text>
       <Input label="Placa"/>
       <Input label="Modelo"/>
